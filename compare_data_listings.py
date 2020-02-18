9c4942b03be0624ee186c68b4eebad4a0e453f07
daviddempsey <davidgdempsey@gmail.com>
2020-02-18 15:56:35 -0800

    first commit

---
+++ compare_data_listings.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+from config import *
+from scripts import *
+import sys
+import subprocess
+
+# compare_data_listings.py
+# Created by David Dempsey
+# email: ddempsey@ucsd.edu
+#
+# ## Purpose:
+# Compares data listings on two different servers
+#
+# ## Usage:
+# [cruise ID] will compare data listings for the cruise ID
+# -v will output process updates to the console
+
+
+args = sys.argv
+
+
+def compare_data_listings(cruise):  # compares listings on different servers
+    ship_abbreviation = get_ship_abbreviation(cruise.upper())
+    org = ''
+    if ship_abbreviation in org_from_cruise.keys():
+        org = org_from_cruise[ship_abbreviation]
+    elif cruise == 'ocport':
+        org = 'OSU'
+    if org == 'UAF':  # for Sikuliaq cruises
+        UAF_process = subprocess.Popen(
+            'rsync -r share.sikuliaq.alaska.edu::SKQDATA/{}'.format(cruise),
+            stdout=subprocess.PIPE, shell=True)
+        UAF_output, err = UAF_process.communicate()  # lists all UAF files
+        SIO_process = subprocess.Popen(
+            "ls -ltraR /localdisk2/R2R/data/Sikuliaq/{} |egrep -v \
+'\.$|\.\.|\.:|\.\/|total|^d' |sed '/^$/d'".format(
+                cruise), stdout=subprocess.PIPE, shell=True)  # lists full tree
+        SIO_output, err = SIO_process.communicate()  # lists all SIO files
+        if len(UAF_output.split('\n')) == len(SIO_output.split('\n')):
+            if '-v' in args:
+                print(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      'The data listings match up for {}'.format(cruise))
+                print(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      '{} file count on local system: {}'.format(cruise,
+                      len(SIO_output.split('\n'))))
+                print(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      '{} file count on UAF system: {}'.format(cruise,
+                       len(UAF_output.split('\n'))))
+            log.write(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      'The data listings match up for {}\n'.format(cruise))
+            log.write(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      '{} file count on local system: {}'.format(cruise,
+                      len(SIO_output.split('\n'))))
+            log.write(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      '{} file count on UAF system: {}'.format(cruise,
+                      len(UAF_output.split('\n'))))
+        else:  # runs if the number of files is NOT the same
+            if '-v' in args:
+                print('ERROR: ' +
+                      datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      'The data listings \
+                      DO NOT match up for {}'.format(cruise))
+                print(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      '{} file count on local system: {}'.format(cruise,
+                      len(SIO_output.split('\n'))))
+                print(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      '{} file count on UAF system: {}'.format(cruise,
+                      len(UAF_output.split('\n'))))
+            log.write('ERROR: ' +
+                      datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      'The data listings \
+                      DO NOT match up for {}\n'.format(cruise))
+            log.write(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      '{} file count on local system: {}'.format(cruise,
+                      len(SIO_output.split('\n'))))
+            log.write(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      '{} file count on UAF system: {}'.format(cruise,
+                      len(UAF_output.split('\n'))))
+    if org == 'OSU':  # for Oceanus cruises
+        OSU_process = subprocess.Popen(
+            'rsync -r \
+r2r@untangle.coas.oregonstate.edu://{}/{}'.format(datadir_OSU, cruise),
+            stdout=subprocess.PIPE, shell=True)
+        OSU_output, err = OSU_process.communicate()  # lists all OSU files
+        SIO_process = subprocess.Popen(
+            "ls -ltraR /localdisk2/R2R/data/Oceanus/{} |egrep -v \
+'\.$|\.\.|\.:|\.\/|total|^d' |sed '/^$/d'".format(
+                cruise),
+            stdout=subprocess.PIPE, shell=True)  # lists full tree
+        SIO_output, err = SIO_process.communicate()  # lists all SIO files
+        if len(OSU_output.split('\n')) == len(SIO_output.split('\n')):
+            if '-v' in args:
+                print(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      'The data listings match up for {}'.format(cruise))
+                print(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      '{} file count on local system: {}'.format(cruise,
+                      len(SIO_output.split('\n'))))
+                print(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      '{} file count on OSU system: {}'.format(cruise,
+                      len(OSU_output.split('\n'))))
+            log.write(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      'The data listings match up for {}\n'.format(cruise))
+            log.write(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      '{} file count on local system: {}'.format(cruise,
+                      len(SIO_output.split('\n'))))
+            log.write(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      '{} file count on OSU system: {}'.format(cruise,
+                      len(OSU_output.split('\n'))))
+        else:
+            if '-v' in args:
+                print('ERROR: ' +
+                      datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      'The data listings \
+                      DO NOT match up for {}'.format(cruise))
+                print(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      '{} file count on local system: {}'.format(cruise,
+                      len(SIO_output.split('\n'))))
+                print(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      '{} file count on OSU system: {}'.format(cruise,
+                      len(OSU_output.split('\n'))))
+            log.write('ERROR: ' +
+                      datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      'The data listings \
+                      DO NOT match up for {}\n'.format(cruise))
+            log.write(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      '{} file count on local system: {}'.format(cruise,
+                      len(SIO_output.split('\n'))))
+            log.write(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                      '{} file count on OSU system: {}'.format(cruise,
+                      len(OSU_output.split('\n'))))
+    else:
+        if '-v' in args:
+            print('ERROR: ' + datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                  'Cannot compare data listings for {}'.format(cruise))
+        log.write('ERROR: ' + datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+                  'Cannot compare data listings for {}\n'.format(cruise))
+
+
+def compare_from_list(list):  # compares a list of cruises
+    file = open('/localdisk2/R2R/code/{}'.format(list), 'r')  # opens list
+    list = [line.rstrip('\n') for line in file]  # splits cruises into a list
+    if '-v' in args:
+        print(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+              "Running data listing comparison")
+    log.write(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+              "Running data listing comparison\n")
+    for each in list:
+        compare_data_listings(each)
+    if '-v' in args:
+        print(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+              "Script finished executing")
+    log.write(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+              "Script finished executing\n")
+
+
+cruise = ''
+
+for i in range(len(args)):  # finds cruise in args
+    if args[i][0] != '-' and args[i][0] != '.' and args[i][0] != '/':
+        cruise = args[i]
+
+log = open(log_dir + datetime.now().strftime('%Y-%m-%dT%H:%M:%S_')
+           + 'compare_data_listings.py_' + cruise + '.txt', 'w+')
+log.write(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+          'compare_data_listings.py executed\n')
+
+if '-l' in args:  # checks to see if script needs to run from list
+    list = 'list.txt'
+    for i in range(len(args)):
+        if args[i][0] != '-' and args[i][0] != '.' and args[i][0] != '/':
+            list = args[i]
+    compare_from_list(list)
+else:
+    compare_data_listings(cruise)
---
+++ get_cruise.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+import os
+from datetime import datetime
+from config import *
+from scripts import *
+import sys
+import logging
+
+# get_cruise.py
+# Created by David Dempsey
+# email: ddempsey@ucsd.edu
+#
+# ## Purpose:
+# Pulls cruise data locally
+#
+# ## Usage:
+# [cruise id] runs a cruise ID (ex: RR1801)
+# -l [no argument] pulls all cruises off of a list (in /code/)
+# (default list: list.txt)
+# -a lists all cruises under an abbreviation (ex: "SP")
+# -v will output logs to the console
+# -h shows what each flag does
+
+args = sys.argv
+
+def get_cruise(cruise):  # runs rsync based off of a cruise ID
+    # finds ship and org based off of cruise
+    ship, org = auto_identify(cruise)
+
+    logging.info(cruise + ' labelled as ' + ship)
+
+    # checks to see if already run
+    exists = os.path.isdir(datadir_local + '/' + ship + '/' + cruise)
+
+    # logs if cruise already exists
+    if exists and '-o' not in args:
+        logging.info(datadir_local + '/' + cruise + ' already exists')
+    else: # otherwise runs rsync
+        rsync_cruises(cruise, ship, org)
+
+
+def run_cruise_ID(cruise): # verifies cruise ID before running rsync
+    ship_abbreviation = get_ship_abbreviation(cruise) # gets cruise identifier
+
+    if '-v' in args:
+        print("Running inputted cruise ID")
+
+    # checks validity of cruise ID
+    if ship_abbreviation.upper() in ships.keys(): # will run rsync if valid
+        get_cruise(cruise)
+        if '-v' in args: # indicates that rsync is finished
+            print("Finished running cruise ID")
+
+    else: # invalid cruise
+        if '-v' in args:
+            print("Incorrect cruise ID")
+
+
+def identify_datadir(org): # identifies cruise organization data directory
+    datadir = ''
+    if org == 'SIO':
+        datadir = datadir_SIO
+    if org == 'OSU':
+        datadir = datadir_OSU
+    if org == 'UAF':
+        datadir = datadir_UAF
+    return datadir # returns string with organization data directory
+
+
+def identify_localdir(ship): # identifies cruise organization data directory
+    localdir = ''
+    if ship == 'Healy':
+        localdir = ship_directory[ship]
+    return localdir # returns string with organization data directory
+
+
+def rsync_cruises(cruise, ship, org): # runs rsync
+    datadir = identify_datadir(org) # finds org data directory
+
+    rsync_txt_check, rsync_cruise_check = (0, 0) # used to log errors
+
+    # runs rsync for SIO
+    if org == 'SIO':
+        localdir = identify_localdir(ship)
+        rsync_txt_check = os.system(rsync_by_org[org][0].format(
+            ship_title=ship, cruise_title=cruise, dir=localdir))
+        rsync_cruise_check = os.system(
+            rsync_by_org[org][1].format(ship_title=ship,
+                                        cruise_title=cruise, dir=localdir))
+
+    # runs rsync for Oregon State
+    if org == 'OSU':
+        rsync_cruise_check = os.system(
+            rsync_by_org[org].format(cruise_title=cruise, dir=datadir)
+            + " > /mnt/gdc/code/scripts/errorChecking")
+
+    # runs rsync for UAF
+    if org == 'UAF':
+        rsync_cruise_check = os.system(
+            rsync_by_org[org].format(ship_title=ship, dir=datadir_SIO,
+                                     skq_dir=datadir, cruise_title=cruise))
+
+    # logs that error occured
+    if rsync_cruise_check != 0:
+        if '-v' in args:
+            print('ERROR: rsync error occurred running ' +
+                  rsync_by_org[org][1].format(ship_title=ship,
+                                              cruise_title=cruise,
+                                              dir=datadir))
+        logging.error('rsync error occurred running ' +
+                      rsync_by_org[org][1].format(ship_title=ship,
+                                                  cruise_title=cruise,
+                                                  dir=datadir))
+
+    # logs that rsync successfully ran
+    if rsync_txt_check == 0 and rsync_cruise_check == 0:
+        if '-v' in args:
+            print('rsync successfully executed for cruise ' + cruise)
+        logging.info('rsync successfully executed for cruise ' + cruise)
+
+
+def read_list(list, function, args):
+    os.chdir(codedir);
+    file = open('{}'.format(list), 'r')  # opens list of cruises
+    list = [line.rstrip('\n') for line in file]   # splits cruises into a list
+    if '-v' in args:
+        print(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+              "Pulling cruises from list")
+    for cruise in list:
+        function(cruise)
+    if '-v' in args:
+        print(datetime.now().strftime('%Y-%m-%dT%H:%M:%S: ') +
+              "Script finished executing")
+
+cruise = ''
+for i in range(len(args)): # extracts cruise ID from arguments
+    if args[i] not in unread_args and args[i] != 'get_cruise.py':
+        cruise = args[i]
+
+os.chdir(log_dir) # creates log file
+#logging.basicConfig(format='%(asctime)s %(message)s', filename='%s' %
+ #                   (datetime.now().strftime('%Y-%m-%dT%H:%M:%S_') +
+  #                   'get_cruise.py_' + cruise),
+   #                 level=logging.INFO)
+logging.info('get_cruise.py executed')
+
+# parses command line arguments
+if len(args) > 1:
+    if '-l' in args: # runs list of cruises
+        list = 'list.txt'
+        for i in range(len(args)):
+            if args[i] == "-l":
+                list = args[i+1]
+        read_list(list, get_cruise, args)
+
+    elif '-a' in args: # prints which cruises can be rsynced
+        list_from_abbreviation(args)
+
+    elif '-h' in args: # outputs usage information
+        print('Input an argument: ')
+        print('-l runs from list')
+        print('[cruise_id] runs a cruise ID')
+        print('-a [ship abbreviation] lists all logged cruises by ship')
+        print('-v will output process updates to the console')
+
+    else: # runs cruise ID if no other args
+        run_cruise_ID(cruise)
+
+else: # outputs usage information if no args given
+    print('Input an argument: ')
+    print('-l runs from list')
+    print('[cruise_id] runs a cruise ID')
+    print('-a [ship abbreviation] lists all logged cruises by ship')
+    print('-v will output process updates to the console')
